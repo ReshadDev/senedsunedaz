6344ad6b79173e4d79c5d0c77bbe73d3b6d51751
ReshadDev <reshadqarayev44@gmail.com>
2024-02-21 13:07:41 +0400

    update-search-page-pagination

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,7 +1,13 @@
 import React from "react";
-import { ErizeSnapshot, noSearch } from "../assets/icons";
+import {
+  CaretLeft,
+  CaretRight,
+  ErizeSnapshot,
+  noSearch,
+} from "../assets/icons";
 import { Link } from "react-router-dom";
 import ErizeExamples from "../data";
+import ReactPaginate from "react-paginate";
 
 const Search: React.FC = () => {
   const documentBoxes = Array(12)
@@ -32,6 +38,20 @@ const Search: React.FC = () => {
       </div>
     ));
 
+  const [currentPage, setCurrentPage] = React.useState(0);
+
+  const handlePageChange = (selectedPage: { selected: number }) => {
+    setCurrentPage(selectedPage.selected);
+  };
+
+  const itemsPerPage = 8; // Change this value based on your preference
+  const offset = currentPage * itemsPerPage;
+
+  const displayedDocumentBoxes = documentBoxes.slice(
+    offset,
+    offset + itemsPerPage
+  );
+
   return (
     <div id="search">
       <main id="maincontent" className="content">
@@ -51,7 +71,9 @@ const Search: React.FC = () => {
                 )}
               </div>
               {documentBoxes.length > 0 ? (
-                <div className="search-results-boxes">{documentBoxes}</div>
+                <div className="search-results-boxes">
+                  {displayedDocumentBoxes}
+                </div>
               ) : (
                 <div className="search-not-found">
                   <div className="search-not-found-icon">
@@ -59,6 +81,22 @@ const Search: React.FC = () => {
                   </div>
                 </div>
               )}
+
+              <div className="pagination">
+                {documentBoxes.length > itemsPerPage && (
+                  <ReactPaginate
+                    previousLabel={<img src={CaretLeft} alt="Previous" />}
+                    nextLabel={<img src={CaretRight} alt="Next" />}
+                    breakLabel={"..."}
+                    pageCount={Math.ceil(documentBoxes.length / itemsPerPage)}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    onPageChange={handlePageChange}
+                    containerClassName={"pagination"}
+                    activeClassName={"active"}
+                  />
+                )}
+              </div>
             </div>
           </div>
         </div>
