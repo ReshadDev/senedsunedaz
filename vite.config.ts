68f3a08b03f8e07a5d662c19979eabf1735ae756
ReshadDev <reshadqarayev44@gmail.com>
2024-03-17 12:42:21 +0400

    sss

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,6 @@
-import { defineConfig, Plugin } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const excludeDocxPlugin: Plugin = {
-  name: "exclude-docx",
-  enforce: "pre",
-  apply: "build",
-  config() {
-    return {
-      resolve: {
-        conditions: ["!*.docx"],
-      },
-    };
-  },
-};
-
 export default defineConfig({
-  plugins: [react(), excludeDocxPlugin],
-  assetsInclude: "**/*.docx",
+  plugins: [react()],
 });
